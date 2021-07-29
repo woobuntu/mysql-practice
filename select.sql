2a694b33e008a39ee481ca227afb83d0470b9af6
in uh, lee <boutime2017@gmail.com>
2021-07-29 13:03:48 +0900

    select challenge

---
+++ select.sql
@@ -0,0 +1,14 @@
+USE mysql_practice;
+SELECT cat_id
+FROM cats;
+SELECT name,
+  breed
+FROM cats;
+SELECT name,
+  age
+FROM cats
+WHERE breed = 'Tabby';
+SELECT cat_id,
+  age
+FROM cats
+WHERE cat_id = age;

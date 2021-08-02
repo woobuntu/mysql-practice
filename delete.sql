30bca01a3e5a2bad9159895dc2de3b9570932620
in uh, lee <boutime2017@gmail.com>
2021-08-02 14:03:34 +0900

    delete challenges

---
+++ delete.sql
@@ -0,0 +1,14 @@
+USE mysql_practice;
+SELECT *
+FROM cats;
+DELETE FROM cats
+WHERE age = 4;
+SELECT *
+FROM cats;
+DELETE FROM cats
+WHERE age = cat_id;
+SELECT *
+FROM cats;
+DELETE FROM cats;
+SELECT *
+FROM cats;
